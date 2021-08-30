5b8f7a487f8d446ee44039458f12fc443b7b47b3
Julian Rolon <julianrolon7@gmail.com>
2021-08-30 01:07:19 -0300

    add-audioplayer-and-new-ui

--- js/audio.js
+++ js/audio.js
@@ -2,12 +2,21 @@ const file = document.getElementById("fileupload");
 
 const container = document.getElementsByClassName("container");
 const canvas = document.getElementById("canvas");
+let audio = document.getElementById("audio1");
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 const ctx = canvas.getContext("2d");
 const audioContext = new AudioContext();
 let audioSource;
 let analyser;
+let currentMode;
+
+let barWidth = 8;
+let barHeight;
+let barSpace = 2;
+let r = 125;
+let g = 125;
+let b = 125;
 
 // btn1.addEventListener("click", playSound);
 
@@ -47,35 +56,23 @@ let analyser;
 // let audios = document.getElementById("audio1");
 // let audio1 = document.getElementById("audio1");
 
-// let micToggle = () => {
-//     let toggler = getElementById("mic-button");
-//     toggler.addEventListener("click", {
-//         change() {
-//             if (audio1 === audios) {
-//                 toggler.classList.toggle("mic-on");
-//                 audio1 = mic;
-//                 let analyser = ctx.createAnalyser();
-//                 let osc = ctx.createOscillator();
-//                 audio.connect(analyser);
-//                 osc.connect(ctx.destination);
-//                 osc.start(0);
+let micToggleBtn = document.getElementById("micBtn");
 
-//                 var data = new Uint8Array(analyser.frequencyBinCount);
-//             } else {
-//                 toggler.classList.toggle("mic-on");
-//                 audio1 = audios;
-//             }
-//         },
-//     });
-// };
+micToggleBtn.addEventListener("click", () => {
+    micToggleBtn.classList.toggle("active");
+});
+
+let songTitle = document.getElementById("title-container");
+let songArtist = document.getElementById("artist-container");
 
 file.addEventListener("change", function () {
     const files = this.files;
-    let audio1 = document.getElementById("audio1");
-    audio1.src = URL.createObjectURL(files[0]);
-    audio1.load();
-    audio1.play();
-    audioSource = audioContext.createMediaElementSource(audio1);
+    songTitle.innerHTML = files[0].name;
+    console.log(files[0]);
+    audio.src = URL.createObjectURL(files[0]);
+    audio.load();
+    // audio.play();
+    audioSource = audioContext.createMediaElementSource(audio);
     analyser = audioContext.createAnalyser();
     audioSource.connect(analyser);
     analyser.connect(audioContext.destination);
@@ -83,43 +80,189 @@ file.addEventListener("change", function () {
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
 
-    const barWidth = 20;
-    let barHeight;
     let x;
-
     function animate() {
         x = 0;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         analyser.getByteFrequencyData(dataArray);
-        drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray);
+        drawVisualiserMirror(bufferLength, x, barWidth, barHeight, dataArray);
         requestAnimationFrame(animate);
     }
+
     animate();
 });
 
-function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray) {
+// SIMPLE BARS
+
+// function drawVisualiserSimple(bufferLength, x, barWidth, barHeight, dataArray) {
+//     for (let i = 0; i < bufferLength; i++) {
+//         barHeight = dataArray[i] * 0.8;
+//         r = (i * barHeight) / 20;
+//         g = barHeight / 2;
+//         b = barHeight * 1.2;
+//         ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+//         ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+//         x += barWidth + barSpace;
+//     }
+// }
+
+// GRAN BARS
+
+// function drawVisualiserGran(bufferLength, x, barWidth, barHeight, dataArray) {
+//     let y;
+//     for (let i = 0; i < bufferLength; i++) {
+//         barHeight = dataArray[i];
+//         r = (i * barHeight) / 20;
+//         g = barHeight / 2;
+//         b = barHeight * 1.2;
+//         ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+//         let miniBarHeight = 15;
+//         for (
+//             y = canvas.height;
+//             y > canvas.height - barHeight;
+//             y -= miniBarHeight + barSpace
+//         ) {
+//             ctx.fillRect(x, y, barWidth, miniBarHeight);
+//         }
+//         x += barWidth + barSpace;
+//     }
+// }
+
+//MIRROR BARS
+
+function drawVisualiserMirror(bufferLength, x, barWidth, barHeight, dataArray) {
     for (let i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i] * 0.8;
-        let r = (i * barHeight) / 20;
-        let g = barHeight / 2;
-        let b = barHeight * 1.2;
+        barHeight = dataArray[i] * 0.5;
+        r = (i * barHeight) / 20;
+        g = barHeight / 2;
+        b = barHeight * 1.2;
+        let y = canvas.height / 2;
         ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-        x += barWidth;
+        ctx.fillRect(x, y, barWidth, barHeight);
+        ctx.fillRect(x, y, barWidth, -barHeight);
+        x += barWidth + barSpace;
     }
 }
 
-// function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray) {
-//     for (let i = 0; i < bufferLength; i++) {
-//         barHeight = dataArray[i] * 0.5;
-//         let r = (i * barHeight) / 20;
-//         let g = barHeight / 2;
-//         let b = barHeight * 1.2;
-//         let ydown = canvas.height / 2;
-//         let yup = canvas.height / 2 ;
-//         ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-//         ctx.fillRect(x, ydown, barWidth, barHeight);
-//         ctx.fillRect(x, yup, barWidth, -barHeight);
-//         x += barWidth;
+// Slider control points
+
+let sizeP = document.getElementById("sizeP");
+let redP = document.getElementById("redP");
+let greenP = document.getElementById("greenP");
+let blueP = document.getElementById("blueP");
+
+sizeP.addEventListener("input", function () {
+    barWidth = parseInt(sizeP.value);
+});
+
+redP.addEventListener("input", function () {
+    r = parseInt(redP.value);
+});
+
+greenP.addEventListener("input", function () {
+    g = parseInt(greenP.value);
+});
+
+blueP.addEventListener("input", function () {
+    b = parseInt(blueP.value);
+});
+
+//Audio Player
+/** Implementation of the presentation of the audio player */
+
+const playIconContainer = document.getElementById("play-icon");
+// const audioPlayerContainer = document.getElementById('audio-player-container');
+const seekSlider = document.getElementById("seek-slider");
+// const volumeSlider = document.getElementById("volume-slider");
+// const muteIconContainer = document.getElementById("mute-icon");
+let playState = "play";
+let muteState = "unmute";
+
+playIconContainer.addEventListener("click", () => {
+    if (playState === "play") {
+        audio.play();
+        playIconContainer.innerHTML = "pause";
+        playState = "pause";
+    } else {
+        audio.pause();
+        playIconContainer.innerHTML = "play_arrow";
+        playState = "play";
+    }
+});
+
+// muteIconContainer.addEventListener("click", () => {
+//     if (muteState === "unmute") {
+//         muteAnimation.playSegments([0, 15], true);
+//         audio.muted = true;
+//         muteState = "mute";
+//     } else {
+//         muteAnimation.playSegments([15, 25], true);
+//         audio.muted = false;
+//         muteState = "unmute";
 //     }
+// });
+
+/** Implementation of the functionality of the audio player */
+
+const durationContainer = document.getElementById("duration");
+const currentTimeContainer = document.getElementById("current-time");
+// const outputContainer = document.getElementById("volume-output");
+let raf = null;
+
+const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${minutes}:${returnedSeconds}`;
+};
+
+const displayDuration = () => {
+    durationContainer.textContent = calculateTime(audio.duration);
+};
+
+const setSliderMax = () => {
+    seekSlider.max = Math.floor(audio.duration);
+};
+
+function whilePlaying() {
+    seekSlider.value = Math.floor(audio.currentTime);
+    currentTimeContainer.textContent = calculateTime(seekSlider.value);
+    raf = requestAnimationFrame(whilePlaying);
+}
+
+// function metaDataLoad () {
+
 // }
+
+if (audio.readyState > 0) {
+    displayDuration();
+    setSliderMax();
+} else {
+    audio.addEventListener("loadedmetadata", () => {
+        displayDuration();
+        setSliderMax();
+    });
+}
+
+audio.addEventListener("play", whilePlaying());
+
+seekSlider.addEventListener("input", () => {
+    currentTimeContainer.textContent = calculateTime(seekSlider.value);
+    if (!audio.paused) {
+        cancelAnimationFrame(raf);
+    }
+});
+
+seekSlider.addEventListener("change", () => {
+    audio.currentTime = seekSlider.value;
+    if (!audio.paused) {
+        requestAnimationFrame(whilePlaying);
+    }
+});
+
+// volumeSlider.addEventListener("input", (e) => {
+//     const value = e.target.value;
+
+//     outputContainer.textContent = value;
+//     audio.volume = value / 100;
+// });
--- js/main.js
+++ js/main.js
@@ -3,45 +3,54 @@ const pageContent = document.getElementById("content");
 //NavBar
 
 const navbar = document.getElementById("navbar");
-navbar.addEventListener("mouseenter", function () {
-    navbar.classList.remove("collapsed");
+const settingsBtn = document.getElementById("settingsBtn");
+settingsBtn.addEventListener("click", function () {
+    navbar.classList.toggle("collapsed");
+    pageContent.classList.toggle("resize-settings");
 });
 
-navbar.addEventListener("mouseleave", function () {
-    navbar.classList.add("collapsed");
-});
+// settingsBtn.addEventListener("click", function () {
+//     navbar.classList.add("collapsed");
+// });
 
 //Modal
-function generateModal() {
-    pageContent.innerHTML += '<div id="modal" class="content_modal">Hola</div>';
-    const modal = document.getElementById("modal");
-}
+
+const modal = document.getElementById("modal");
 
 const newPatternBtn = document.getElementById("addPattern");
 
-// newPatternBtn.addEventListener("click", function () {
-//     generateModal();
-//     modal.innerHTML = `
-//                     <div class="modal_form">
-//                         <label for="name">Name: </label>
-//                         <input type="text" />
-//                         <label for="color">Color: </label>
-//                         <div id="pickerContainer"></div>
-//                         <label for="size">Size: </label>
-//                         <input type="" />
-//                         <label for="form">Form: </label>
-//                         <input type="text" />
-//                         <button type="submit">Create New Pattern</button>
-//                     </div> `;
-//     let color_pickers = [
-//         new ColorPickerControl({
-//             container: document.getElementById("pickerContainer"),
-//         }),
-//     ];
-// });
+newPatternBtn.addEventListener("click", function () {
+    if (modal.innerHTML == "") {
+        modal.innerHTML = `
+                    <form class="modal_form">
+                        <div class="modal_nameP">
+                            <label for="name">Name </label>
+                            <input class="modal_input--text" type="text" />
+                        </div>
+                        <div class="modal_colorP">
+                            <label for="color">Color </label>
+                            <div id="modal_pickerContainer"></div>
+                        </div>
+                        <div class="modal_sizeP">
+                            <label for="size">Size </label>
+                            <input class="modal_input--text" type="" />
+                        </div>
+                        <div class="modal_formP">
+                            <label for="form">Form </label>
+                            <input class="modal_input--text" type="text" />
+                        </div>
 
-let color_pickers = [
-    new ColorPickerControl({
-        container: document.getElementById("pickerContainer"),
-    }),
-];
+                        <button
+                            id="modal_newPatBtn"
+                            class="modal_btn"
+                            type="submit"
+                        >
+                            Create New Pattern
+                        </button>
+                    </form> `;
+
+        modal.style.visibility = "visible";
+
+        modal.classList.toggle("modal--close");
+    }
+});
